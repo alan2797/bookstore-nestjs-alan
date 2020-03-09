f49ef5582cd034794f39a8a44fec2edb29287f02
vladimir <vasquezvladimir169@gmail.com>
2020-03-09 18:35:11 -0400

    vladimir

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { RoleService } from './role.service';
+import { RoleRepository } from './role.repository';
+import { RoleController } from './role.controler';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([RoleRepository])],
+  providers: [RoleService],
+  controllers: [RoleController],
+})
 export class RoleModule {}
